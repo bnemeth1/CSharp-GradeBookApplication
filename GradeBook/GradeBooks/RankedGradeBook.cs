bda5c8c480cd19142c09e3d35c364f368b78d366
bnemeth1 <47466646+bnemeth1@users.noreply.github.com>
2019-02-08 22:21:09 -0500

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,39 @@ namespace GradeBook.GradeBooks
         {
             this.Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+
+            int twentyPctStudents = (int) Math.Ceiling(Students.Count * .2);
+            int countA = twentyPctStudents;
+            int countB = countA + twentyPctStudents;
+            int countC = countB + twentyPctStudents;
+            int countD = countC + twentyPctStudents;
+
+            int countover = 0;
+
+            foreach (var student in Students )
+            {
+                if (student.AverageGrade > averageGrade)
+                {
+                    countover++;
+                }
+            }
+
+            if (countover <= countA)
+                return 'A';
+            else if (countover <= countB)
+                return 'B';
+            else if (countover <= countC)
+                return 'C';
+            else if (countover <= countD)
+                return 'D';
+            
+            return 'F';
+
+        }
     }
 }
